a63595197f8b166a60c81af530e0d1ac148e895f
Kaustubh <kaustubhthakur76@gmail.com>
2023-04-14 13:59:43 +0530

    password generator

--- Basics/control_flow.py
+++ Basics/control_flow.py
@@ -22,9 +22,9 @@
     #print(i)  
 
 #pass statements/while loop
-i = 1
-while i<6:
-    print(i)
-    if i==2:
-        break
-    i+=1;
+#    i = 1
+#    while i<6:
+ #   print(i)
+ #   if i==2:
+ #   break
+ #   i+=1;
